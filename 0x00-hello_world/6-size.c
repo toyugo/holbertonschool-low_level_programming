accda5f72f3951326051905a08660457728d02d0
olivier <olivier.guyot.cu@gmail.com>
2020-09-16 11:16:45 +0000

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	long long int lli = 10;
 	float f = 10;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
