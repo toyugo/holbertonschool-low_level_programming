ca19f87921b8b3025fe36bae39acde94ef01c18e
olivier <olivier.guyot.cu@gmail.com>
2020-09-16 11:13:09 +0000

    Size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'c';
+	int i = 10;
+	long l = 10;
+	long long int lli = 10;
+	float f = 10;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(f));
+	return(0);
+}
