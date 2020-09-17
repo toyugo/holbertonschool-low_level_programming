8430a6a43a0ed0a166971e961ba21262b8596344
olivier <olivier.guyot.cu@gmail.com>
2020-09-17 15:11:37 +0000

    print 2 while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = '1';
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
