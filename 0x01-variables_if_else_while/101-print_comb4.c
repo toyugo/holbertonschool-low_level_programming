6b4d061f4909eda169cb65ef2cfea1c38d3b83a9
olivier <olivier.guyot.cu@gmail.com>
2020-09-17 18:34:59 +0000

    play with operator

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(voiu)
+{
+	int u;
+	int d;
+	int c;
+
+	c  = '0';
+	while (c <= '9')
+	{
+		d = '0';
+		while (d <= '9')
+		{
+			u = '0';
+			while (u <= '9')
+			{
+				if (c < d && d < u)
+				{
+					putchar(c);
+					putchar(d);
+					putchar(u);
+					if (c != '7' || d != '8' || u != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				u++;
+			}
+			d++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
