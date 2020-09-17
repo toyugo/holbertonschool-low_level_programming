3d883e0c033fa88c0d17db5cb34e71b791facd1f
olivier <olivier.guyot.cu@gmail.com>
2020-09-17 12:55:21 +0000

    test2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,8 @@ int main(void)
 		printf("Last digit of %i is %i and and is less than 6 and not 0\n", n, ld);
 	}
 	if (ld == 0)
+	{
 		printf("Last digit of %i is %i and is 0\n", n, ld);
+	}
 	return (0);
 }
