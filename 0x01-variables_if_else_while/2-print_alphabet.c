373419809c156c33e89105071bd1f34bce77902d
olivier <olivier.guyot.cu@gmail.com>
2020-09-17 14:08:26 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
