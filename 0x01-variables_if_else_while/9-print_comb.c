4ca144be90e2912650b6766ea281f1084dd63b11
olivier <olivier.guyot.cu@gmail.com>
2020-09-17 15:22:48 +0000

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+			putchar(',');
+			putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
