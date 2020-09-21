7d810eb224f9d78c94f7394ba6f0dbecb08104ad
olivier <olivier.guyot.cu@gmail.com>
2020-09-21 19:35:51 +0000

    first commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * jack_bauer - Jack programme
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + h / 10);
+			_putchar('0' + h % 10);
+			_putchar(':');
+			_putchar('0' + m / 10);
+			_putchar('0' + m % 10);
+			_putchar('\n');
+		}
+	}
+}
