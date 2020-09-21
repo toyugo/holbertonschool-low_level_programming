2ea8c055bd9b4c801f085160262cf39b6ec8184a
olivier <olivier.guyot.cu@gmail.com>
2020-09-21 22:46:55 +0000

    fix betty:

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
- * main - entry
- * print_alphabet - print alphabet
+ * _isalpha - _isalpha
+ * @c: value
  * Return: 0
  */
 int _isalpha(int c)
 {
 	int i;
+
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		i = 1;
