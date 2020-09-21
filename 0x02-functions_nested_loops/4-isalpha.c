56dcf41e6c7a7a0b9acad0c06edbbaed2cceb51b
olivier <olivier.guyot.cu@gmail.com>
2020-09-21 16:25:13 +0000

    First commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - entry
+ * print_alphabet - print alphabet
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	int i;
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
+	return (i);
+}
