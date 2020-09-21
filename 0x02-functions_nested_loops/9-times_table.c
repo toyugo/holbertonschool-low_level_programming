cd173ab1895a3f0f95bf6bc47faa2fdce15cf648
olivier <olivier.guyot.cu@gmail.com>
2020-09-21 20:28:34 +0000

    Fixe

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
- * time_table- times_table
+ * times_table- times_table
  *
  * Return: 0
  */
@@ -11,29 +11,33 @@ void times_table(void)
 	int m;
 	int t;
 	int res;
-	
+
 	for (t = 0; t <= 9; t++)
 	{
 		for (m = 0; m <= 9; m++)
 		{
 			res = t * m;
-/*			printf("%i", res);
-*/			
 			if (res < 10)
 			{
-				_putchar(res + '0');
-				_putchar(',');
 				_putchar(' ');
+				_putchar(res + '0');
+				if (m != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar(res / 10 + '0');
 				_putchar(res % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (m != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
-/*		_putchar('\n');
-*/		printf("\n");
+		_putchar('\n');
 	}
 }
