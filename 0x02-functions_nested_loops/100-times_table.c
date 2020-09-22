2890c4fbe2675483dca969b992400da1eab613cb
olivier <olivier.guyot.cu@gmail.com>
2020-09-22 13:49:02 +0000

    Fix3

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_times_table - Time table with n
+ * @n: integer 1
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int res;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (!(n > 15 || n < 0))
+			{
+				res = i * j;
+				if ( j == 0 )
+					printf("%i", res);
+				else if (res < 10)
+					printf("   %i", res);
+				else if (res >= 10 && res < 100)
+					printf("  %i", res);
+				else if (res >= 100 && res < 1000)
+					printf(" %i", res);
+				if (j != n)
+					printf(",");
+			}
+		}
+		if (!(n > 15 || n < 0))
+			printf("\n");
+	}
+}
