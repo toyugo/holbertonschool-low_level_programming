a0a181e8c069e66406de9aba27a9dde260c19213
olivier <olivier.guyot.cu@gmail.com>
2020-09-22 13:33:11 +0000

    Test an other way

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,9 +14,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > c)
+	else if (b > c && b > c)
 	{
 		largest = b;
+	}
+	else if(b == a)
+	{
+		largest = a;
 	}
 	else
 	{
@@ -25,4 +29,3 @@ int largest_number(int a, int b, int c)
 
 	return (largest);
 }
-
