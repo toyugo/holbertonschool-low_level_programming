f5ea2039ae5ca059b3464bdd3b3d7ddf63a2d58e
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 21:03:10 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * print_number - print_number
+ * @n: integer number
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int i;
+	int nbcar;
+	int nbcarpower;
+	int n1;
+
+	nbcar = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	if (n == 0)
+		_putchar('0');
+	n1 = n;
+	while (n % 10 != 0 || n / 10 != 0)
+	{
+		nbcar += 1;
+		n /= 10;
+	}
+	while (nbcar > 0)
+	{
+		nbcarpower = 1;
+		for (i = 1; i < nbcar; i++)
+			nbcarpower = nbcarpower * 10;
+		_putchar((n1 / nbcarpower) % 10 + '0');
+		nbcar -= 1;
+	}
+}
