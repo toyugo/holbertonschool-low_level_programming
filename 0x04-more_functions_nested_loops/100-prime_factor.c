697ed3e66102b7223f65d61de3cf75fad98e92ef
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 17:32:14 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ */
+
+int main(void)
+{
+	long nb;
+	long i;
+
+	nb = 612852475143;
+
+	for (i = 2; i < nb; i++)
+		while (nb % i == 0)
+			nb = nb / i;
+	printf("%lu", nb);
+
+	return (0);
+}
