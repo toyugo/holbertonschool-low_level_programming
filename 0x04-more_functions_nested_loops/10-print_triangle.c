8843e57a4b7b143161d0be4075e32b87d07fcbbd
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 16:26:09 +0000

    Fix

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	k = 1;
+	if (size == 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; size > 0; j++)
+		{
+			for (i = 0; i < size-1; i++)
+			{
+				printf(" ");
+			}
+			size -= 1;
+			for (i = 0; i < k; i++)
+				printf("#");
+			k++;
+			printf("\n");
+		}
+	}
+}
