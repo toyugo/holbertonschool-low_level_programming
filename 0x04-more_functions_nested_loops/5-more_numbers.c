a086c6a7c6735e01a327a2eb87520af177304db5
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 10:39:37 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * more_numbers - more_numbers
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i <= 9)
+				_putchar(i + '0');
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
