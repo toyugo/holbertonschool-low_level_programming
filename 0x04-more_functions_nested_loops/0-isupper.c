816a193c25209c9baed214a1b4f8bf344ef285a8
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 10:03:39 +0000

    Commit 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _isupper - upper
+ * @c
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	int res;
+
+	if (c >= 'A' && c <= 'Z')
+		res = 1;
+	else if (c <= 'a' && c <= 'z')
+		res = 0;
+	return (res);
+}
