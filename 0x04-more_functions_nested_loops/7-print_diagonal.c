d4084b13cd928f1c858da2611c512fda425c8a3e
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 12:35:43 +0000

    First commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,10 +14,10 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 1 ; j <= i; j++)
-				_putchar('.');
+			for (j = 0 ; j < i; j++)
+				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 		}
