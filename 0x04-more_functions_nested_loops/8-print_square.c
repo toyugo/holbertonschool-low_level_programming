debff1f25a8d02ba382137d8db91e60044f02fab
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 12:52:40 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_square - print_square
+ * @n: Number
+ * Return: 0
+ */
+
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (n == 0)
+		_putchar('\n');
+}
