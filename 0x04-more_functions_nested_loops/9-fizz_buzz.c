371f0479bc2fc9b8b5a6deffda8dcccd94c3c21b
Olivier <olivier.guyot.cu@gmail.com>
2020-09-24 13:35:47 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Print fizz buzz
+ * Return: void 
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else printf("%i", i);
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
+
+	}
+	return (0);
+}
