210517446b0baf95a9078a0acef6d2b6dc120049
Olivier <olivier.guyot.cu@gmail.com>
2020-09-26 14:56:36 +0000

    Fix max long

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,44 @@
 
 int main(void)
 {
-	unsigned long i;
-	unsigned long j;
-	unsigned long k;
-	int cp;
+	unsigned long a = 1, b = 2, c = 3, a2 = 0;
+	unsigned long i, b2, c2, a1, b1, c1;
+	unsigned long ncut = 10000000000;
 
-	i = 1;
-	j = 2;
-	printf("1, 2, ");
-	for (cp = 3; cp <= 98; cp++)
+	for (i = 2; i <= 98; i++)
 	{
-		k = i + j;
-		i = j;
-		j = k;
-		if (cp != 98)
-			printf("%lu, ", k);
+		if (a + b > b)
+		{
+			c = a + b;
+			a = b;
+			b = c;
+			printf("%lu ,", c);
+		}
 		else
-			printf("%lu", k);
+		{
+			if (a2 == 0)
+			{
+				a2 = a % ncut;
+				b2 = b % ncut;
+				a1 = a / ncut;
+				b1 = b / ncut;
+			}
+			c1 = a1 + b1;
+			c2 = a2 + b2;
+			if (c2 >= ncut)
+			{
+				c2 = c2 % ncut;
+				c1 += 1;
+			}
+			a1 = b1;
+			b1 = c1;
+			a2 = b2;
+			b2 = c2;
+			if (i != 98)
+				printf("%lu%lu ,", c1, c2);
+			else
+				printf("%lu%lu\n", c1, c2);
+		}
 	}
-	printf("\n");
 	return (0);
 }
