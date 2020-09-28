7d0a7cba21bce05152075c0ccf76ea0c24809997
Olivier <olivier.guyot.cu@gmail.com>
2020-09-28 22:56:28 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+* _strlen - _strlen
+* @s: string
+* Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	;
+	return (i);
+}
+
+/**
+* puts2 - puts2
+* @str: string
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int size;
+
+	size = _strlen(str);
+	for (i = 0; i < size - 1; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
