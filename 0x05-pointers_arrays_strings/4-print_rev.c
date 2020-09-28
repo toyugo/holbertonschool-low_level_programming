cd7555ae7bf81bb7c4aab4d435225cc2a32cd305
Olivier <olivier.guyot.cu@gmail.com>
2020-09-28 16:02:56 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_rev - print_rev
+* @s: string
+* Return: Always 0.
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
