11c121b009085399025853ffc6eacc0732854be5
Olivier <olivier.guyot.cu@gmail.com>
2020-09-28 22:10:33 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* rev_string - rev_string
+* @s: string
+* Return: Always 0.
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int size;
+
+	for (i = 0; s[i] != '\0'; i++)
+	;
+	size = i;
+
+	char c[size + 1];
+
+	for (i = 0; i <= size; i++)
+		c[i] = s[i];
+	for (i = 0; i < size; i++)
+		s[i] = c[size - 1- i];
+}
