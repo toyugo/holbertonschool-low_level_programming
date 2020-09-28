8fee5da2290344e5d0eee78aa2ed6a0c7c15abbc
Olivier <olivier.guyot.cu@gmail.com>
2020-09-28 23:17:23 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+* _strlen - _strlen
+* @s: string
+* Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	;
+	return (i);
+}
+
+/**
+* puts_half - puts_half
+* @str: string
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int size;
+
+	size = _strlen(str);
+	for (i = size / 2; i < size; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
