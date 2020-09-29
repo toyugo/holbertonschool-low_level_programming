225a128ecfdde1e9811d9ceed1e6bdf2a5fd81eb
Olivier <olivier.guyot.cu@gmail.com>
2020-09-29 19:34:10 +0000

    Fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,16 +22,7 @@ int _atoi(char *s)
 	for (; s[i] >= '0' && s[i] <= '9'; i++)
 	;
 	ednb = i;
-/* calculate len number*/
-	lennb = ednb - stnb;
-/* Convert Number */
-	for (i = 1; i < lennb ; i++)
-		coef *= 10;
-	for (i = 0; i < lennb; i++)
-	{
-		res += (s[stnb + i] - '0') * coef;
-		coef /= 10;
-	}
+/* sign */
 	for (i = 0; i < stnb; i++)
 	{
 		if (s[i] == '-')
@@ -39,7 +30,16 @@ int _atoi(char *s)
 			sign = sign * -1;
 		}
 	}
-	if (s[stnb - 1] == '-')
-		res = res * sign;
+/* calculate len number*/
+	lennb = ednb - stnb;
+/* Convert Number */
+	for (i = 1; i < lennb ; i++)
+		coef *= 10;
+	for (i = 0; i < lennb; i++)
+	{
+		res += sign * (s[stnb + i] - '0') * coef;
+		coef /= 10;
+	}
+
 	return (res);
 }
