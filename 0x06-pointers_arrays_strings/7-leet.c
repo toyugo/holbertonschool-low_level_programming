ea4d1e3652693841a00a490d011a28904e27c60e
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 16:35:25 +0000

    Fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,14 +9,14 @@ char *leet(char *s)
 {
 	int i;
 	int j;
-	char tb[6] = "aeotl";
-	char tb2[6] = "43071";
+	char tb[] = "aeotl";
+	char tb2[] = "43071";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 4; j++)
 		{
-			if ((s[i] == tb[j]) || (s[i] == tb[j] - ('A' - 'a')))
+			if ((s[i] == tb[j]) || (s[i] == tb[j] - ('a' - 'A')))
 				s[i] = tb2[j];
 		}
 	}
