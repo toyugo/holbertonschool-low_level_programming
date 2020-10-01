4c20b9de52944bd6eeeef47a3599c7dca8eaa47a
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 14:36:13 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* string_toupper - string_toupper
+* s: pointer
+* Return: char
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= ('a' - 'A');
+	}
+	return (s);
+}
