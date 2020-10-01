3060e968aec5618470a19d851a7b15dd0b287cc8
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 16:29:03 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* leet - leet
+* @s: pointer
+* Return: char
+*/
+
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char tb[] = "aeotl";
+	char tb2[] = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 4; j++)
+		{
+			if ((s[i] == tb[j]) || (s[i] == tb[j] - ('A' - 'a')))
+				s[i] = tb2[j];
+		}
+	}
+	return (s);
+}
