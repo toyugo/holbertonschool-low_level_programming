f1441fee9e7532f2cc1ab607d72180a696d02f04
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 12:43:41 +0000

    First commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,15 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[j] = src[i];
 		j++;
 	}
-	for(j = 0; dest[j] != '\0'; j++)
+	for (j = 0; dest[j] != '\0'; j++)
 	;
 
-	dest[j + 1] = '\0';
-	
+	dest[j + 1] = '\0';	
 	return (dest);
 }
