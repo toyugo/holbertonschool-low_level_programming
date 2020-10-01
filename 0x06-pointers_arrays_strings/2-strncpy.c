0568973558b02ff06acef533c3e3a0f25ec9a598
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 12:55:15 +0000

    First commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include<stdio.h>
 /**
 * _strncpy - _strncpy
 * @dest: destination
@@ -11,16 +10,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 
 	for (i = 0; i < n && src[i]; i++)
-	{
-		dest[j] = src[i];
-		j++;
-	}
-	for (j = 0; dest[j] != '\0'; j++)
-	;
-	for (; i < j; i++)
-		dest[j] = '\0';
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
