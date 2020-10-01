299cdcedffeb7b082ebf4c04b9ba6ede84d20f21
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 13:43:30 +0000

    Fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] == s2[i] && s1[i] && s2[i]; i++)
+	for (i = 0; s1[i] == s2[i] && (s1[i] || s2[i]); i++)
 	;
 
 	return (s2[i] - s1[i]);
