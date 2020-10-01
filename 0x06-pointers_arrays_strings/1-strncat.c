d45138f3c4707016af4950d08d7d87449b3d8276
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 11:27:18 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _strncat - _strncat
+* @dest: destination
+* @src: source
+* n: int
+* Return: char
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for ( i = 0; dest[i] != '\0'; i++)
+	;
+	j = 0;
+	for (; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i + 1] = '\0'; 
+	return (dest);
+}
