72a49f592ee4fb892c1d70a0f4926a3a52f22197
Olivier <olivier.guyot.cu@gmail.com>
2020-10-01 11:38:50 +0000

    Fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 /**
 * _strncat - _strncat
 * @dest: destination
@@ -20,5 +19,6 @@ char *_strncat(char *dest, char *src, int n)
 		dest[i] = src[j];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
