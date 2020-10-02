5f1702d7994ba70d92cf1c184fe879e93f4d14c6
Olivier <olivier.guyot.cu@gmail.com>
2020-10-02 20:07:52 +0000

    Fix

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,12 +1,9 @@
 #include "holberton.h"
 #include<stdio.h>
 /**
-* infinite_add - infinite_add
-* n1: char
-* n2: char
-* r: char
-* size_r: int
-* Return: char
+* str_len - str_len
+* @s: string
+* Return: int
 */
 int str_len(char *s)
 {
@@ -15,27 +12,45 @@ int str_len(char *s)
 	for (i = 0; s[i]; i++)
 	;
 
-	return(i);
+	return (i);
 }
+/**
+* reverse_str - reverse_str
+* @s: string
+* Return: int
+*/
 char *reverse_str(char *s)
 {
 	char tmp;
 	int size;
 	int i;
-	
+
 	size = str_len(s) - 1;
 	for (i = 0; i <= size / 2; i++)
 	{
 		tmp = s[i];
 		s[i] = s[size - i];
-		s[size -i] = tmp;
+		s[size - i] = tmp;
 	}
 	return (s);
 }
+/**
+* ctoint - ctoint
+* @c: char
+* Return: int
+*/
 int ctoint(char c)
-{	
+{
 	return (c - '0');
 }
+/**
+* infinite_add - infinite_add
+* @n1: char
+* @n2: char
+* @r: char
+* @size_r: int
+* Return: char
+*/
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
@@ -43,9 +58,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int j;
 	int k;
 	int ret = 0;
-	int tmpn1=0;
-	int tmpn2=0;
-	int tmptot=0;
+	int tmpn1 = 0;
+	int tmpn2 = 0;
+	int tmptot = 0;
 
 	i = str_len(n1) - 1;
 	j = str_len(n2) - 1;
@@ -59,33 +74,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			tmpn1 = 0;
 		if (j < 0)
 			tmpn2 = 0;
-//		printf("old tmptot = %i\n",tmptot);
 		tmptot = tmpn1 + tmpn2 + ret;
-//		printf("i = %i tmptot new: %i = tmpn1:%i + tmpn2: %i + ret: %i\n",i, tmptot, tmpn1, tmpn2, ret);
 		ret = 0;
 
 		if (tmptot >= 10)
 		{
-//		printf("RETENUE\n");
 			ret = 1;
 			tmptot -= 10;
 		}
-	
 		r[k] = tmptot + '0';
-//		printf("k= %i r[k]= %c ;  n1[i] = %i +  n2[j] = %i\n",k, r[k], n1[i] - '0', n2[j] - '0'); 
-//		printf("decrement i %i", i);
 		i--;
-//		printf("i vault %i\n", i);
-//		printf("decrement j %i", j); 
 		j--;
-//		printf("j vault %i\n", j);
-//		printf("decrement k %i", k); 
 		k++;
-//		printf("k vault %i\n", k);
 	}
 	r[k] = '\0';
 	reverse_str(r);
-//	printf("string de k vault : <%c>", r[0]);
-//	printf("END \n");
 	return (r);
 }
