6e3ddf8b315aed6681c8615c8a1b8cfd07320c9a
Olivier <olivier.guyot.cu@gmail.com>
2020-10-05 16:05:11 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strpbrk - _strpbrk
+* @s: char
+* @accept: char
+* Return: char
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	int b;
+	int b1 = 0;
+
+	for (i = 0; s[i] && b1 == 0; i++)
+	{
+		b  = 0;
+		printf("%i\n", i);
+		for (j = 0; accept[j] && b == 0; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				b = 1;
+				b1 = 1;
+			}
+		}
+	}
+	if (i > 0)
+		i -= 1;
+	return (&s[i]);
+}
