f959d8e8eb5c020eacbadd0e80cbe99f686f1834
Olivier <olivier.guyot.cu@gmail.com>
2020-10-05 10:10:32 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _memset - _memset
+* @s: string
+* @b: char
+* @c unsigned int
+* Return: string
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
