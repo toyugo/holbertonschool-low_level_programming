f8e78feb57584d7e50e2c6fddacab083fafa6b59
Olivier <olivier.guyot.cu@gmail.com>
2020-10-05 19:19:45 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* set_string - set_string
+* @s: pointer to pointer
+* @to : string
+* Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
