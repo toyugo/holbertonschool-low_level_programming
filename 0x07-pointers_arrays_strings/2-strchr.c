82fb7688435a97955e1a51b6e2e3e1ba702b59e4
Olivier <olivier.guyot.cu@gmail.com>
2020-10-05 11:36:33 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strchr - _strchr
+* @s: char
+* @c: char
+* Return: string
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
