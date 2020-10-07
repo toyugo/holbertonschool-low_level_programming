fe415c94fb06a91fee4e82e3aba7683e0e50bfc6
Olivier <olivier.guyot.cu@gmail.com>
2020-10-07 20:27:54 +0000

    First commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * checkprime - checkprime
+ * @n: int
+ * @val: int
+ * Return: int
+ */
+
+int checkprime(int n, int val)
+{
+	if (n == val)
+		return (0);
+	else if (val % n == 0)
+		return (1);
+	return (checkprime(n + 1, val));
+}
+
+/**
+ * is_prime_number - is_prime_number
+ * @n: int
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	return (checkprime(2, n));
+}
+
