703528dd765b6c45cb3d17312af90d4e02dbb52c
Olivier <olivier.guyot.cu@gmail.com>
2020-10-08 22:38:06 +0000

    With real recursive concept

---
+++ 0x08-rework/0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - _print_rev_recursion
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
