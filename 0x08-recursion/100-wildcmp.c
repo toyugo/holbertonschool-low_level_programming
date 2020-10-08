0bc780b8668cbd114dba12392fb32db067952218
Olivier <olivier.guyot.cu@gmail.com>
2020-10-08 20:36:51 +0000

    First commit

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "holberton.h"
 /**
  * wildcmp - wildcmp
  * @s1: string
@@ -7,8 +9,22 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (s1[0] == '\0' && s2[0] == '\0')
+	if (s2[0] == '\0' && s1[0] == '\0')
+	{
 		return (1);
-	
-	return(0);
+	}
+	if (s2[0] == s1[0])
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	/* for multiple * */
+	if (s2[0] == '*' && s1[0] == '\0')
+		return (wildcmp(s1, s2 + 1));
+	/* for multiple char for don't work */
+	if (s2[0] == '*')
+	{
+		if (wildcmp(s1 + 1, s2))
+			return (1);
+	}
+	return (0);
 }
