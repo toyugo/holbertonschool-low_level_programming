a1b1f3b2a945aa18ce103bf92be3acaaf9d5600e
Olivier <olivier.guyot.cu@gmail.com>
2020-10-08 23:42:01 +0000

    rework long to pass int max

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,24 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/**
- * recurs - calculate square
- * @n: integer to decrease to test each number
- * @checkvalue: check the square
- * Return: the square
- */
-int recurs(int n, int checkvalue)
-{
-	if (n <= 0 || n * n > checkvalue)
-		return (-1);
-	if (n * n == checkvalue)
-		return (n);
-	return (recurs(n + 1, checkvalue));
-}
 
-/**
- * _sqrt_recursion - Squart recursively
- * @n: int
- * Return: int
- */
+int _sqrt(int n, long i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (i * i == n)
+			return (i);
+		return (_sqrt(n, i + 1));
+	}
+}
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
-	return (recurs(1, n));
+	if (n <= 0)
+		return (-1);
+	return (_sqrt(n, 0));
 }
