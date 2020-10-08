bba885a07fe2a1357c48a7f477c1daddd582046d
Olivier <olivier.guyot.cu@gmail.com>
2020-10-08 22:45:58 +0000

    Real recursion principle

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,8 +8,13 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (*s == '\0')
+	{
 		return;
-	_print_rev_recursion(s + 1);
-	_putchar(s[0]);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
 }
