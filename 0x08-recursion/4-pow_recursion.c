e1ae7bf90385abe51218392d49cd3c72ec8acff9
Olivier <olivier.guyot.cu@gmail.com>
2020-10-08 23:08:33 +0000

    Rework

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,13 +10,10 @@
 
 int _pow_recursion(int x, int y)
 {
-	int res = 1;
-
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
+	if (y == 0)
 		return (1);
-	res = x * _pow_recursion(x, y - 1);
-
-	return (res);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
