16b7abf9f56069c10a4228f5b72cab32318d1f19
Olivier <olivier.guyot.cu@gmail.com>
2020-10-08 22:57:46 +0000

    Rework

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int res = 0;
-
-	if (s[0] == '\0')
+	if (*s == '\0')
 		return (0);
-	s = s + 1;
-	res = _strlen_recursion(s) + 1;
-	return (res);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
