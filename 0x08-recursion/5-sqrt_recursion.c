ab70c430a730d2467c7bb81d01f386bfed68bcf7
Olivier <olivier.guyot.cu@gmail.com>
2020-10-08 23:45:23 +0000

    Betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,12 @@
 #include "holberton.h"
 #include <stdio.h>
 
-
+/**
+ * _sqrt - square with recursive loop
+ * @n: constant n;
+ * @i: i long to pass int max
+ * Return: square result
+ */
 int _sqrt(int n, long i)
 {
 	if (i * i > n)
@@ -15,6 +20,11 @@ int _sqrt(int n, long i)
 		return (_sqrt(n, i + 1));
 	}
 }
+/**
+ *_sqrt_recursion - original function
+ * @n: number
+ * Return: square result
+ */
 
 int _sqrt_recursion(int n)
 {
