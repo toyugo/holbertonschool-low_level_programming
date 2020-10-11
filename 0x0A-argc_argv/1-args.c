eb04d614049da891b64f73436fbfb78f36fbfa9a
Olivier <olivier.guyot.cu@gmail.com>
2020-10-11 15:28:43 +0000

    First commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - main
+ * @argc: main arg
+ * @argv: main arg
+ * Return: c
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
