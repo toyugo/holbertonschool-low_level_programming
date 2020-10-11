32880f9f210cd72081d5f7e9d45ec62c08a97c0b
Olivier <olivier.guyot.cu@gmail.com>
2020-10-11 15:18:37 +0000

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
