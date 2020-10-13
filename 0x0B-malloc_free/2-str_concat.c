7947a91f61ed9116f7ff5160450371faa5f95aab
Olivier <olivier.guyot.cu@gmail.com>
2020-10-13 11:41:39 +0000

    Fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,9 +32,9 @@ char *str_concat(char *s1, char *s2)
 
 	s = malloc(sizeof(char) * (_strlen(s1)  + _strlen(s2) + 1));
 	if (s1 == NULL)
-		s1 = NULL;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = NULL;
+		s2 = "";
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
