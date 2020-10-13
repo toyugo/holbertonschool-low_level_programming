6132f6c7fd724010951b752b8b1f2f9f211d14a1
Olivier <olivier.guyot.cu@gmail.com>
2020-10-13 10:24:06 +0000

    First commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 	ar = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 		ar[i] = c;
