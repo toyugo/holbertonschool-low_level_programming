dc38c69a7289042ca09cc840e3d67e7e45ea241a
Olivier <olivier.guyot.cu@gmail.com>
2020-10-13 10:21:28 +0000

    First commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * create_array - create_array
+ * @size: int
+ * @c: char
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	ar = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+		ar[i] = c;
+
+	return (ar);
+}
