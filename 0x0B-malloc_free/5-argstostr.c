9d6950e724573e52104a41f151a72a507b779bb1
Olivier <olivier.guyot.cu@gmail.com>
2020-10-13 14:55:48 +0000

    First commit

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * argstostr - argstostr
+ * @ac: int
+ * @av: char
+ * Return: char
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int len = 0;
+	char *s;
+
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+	}
+	s = malloc(len + 1 * sizeof(char) + ac);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		s[k] = '\n';
+		k++;
+	}
+	s[k] = '\0';
+
+	return (s);
+}
