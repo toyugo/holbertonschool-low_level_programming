a17e8cf2fe2655b06964f89cfbd20bdcf5a4058a
Olivier <olivier.guyot.cu@gmail.com>
2020-10-15 10:24:17 +0000

    First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * malloc_checked - malloc_checked
+ * @b: unsigned
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{	
+	unsigned long long *c;
+
+	c = malloc(b);
+	if (c == NULL)
+		exit(98);
+
+	return (c);
+}
