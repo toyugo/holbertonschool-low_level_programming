e7e65626b179b235ce2a1e9f8d97b1a4add48966
Olivier <olivier.guyot.cu@gmail.com>
2020-10-15 12:40:38 +0000

    First commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strlen - _strlen
+ * @s: char
+ * Return: char *
+ */
+int _strlen(char *s)
+{
+	int k;
+
+	for (k == 0; s[k] != '\0'; k++)
+	;
+	return (k);
+}
+
+/**
+ * string_nconcat - string_nconcat
+ * @s1: char
+ * @s2: char
+ * @n : unsigned int
+ * Return: char *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3;
+	int i;
+	int j;
+
+	j = 0;
+	s3 = malloc(sizeof(s3) * (_strlen(s1) + _strlen(s2) + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s3[j] = s1[i];
+		j++;
+	}
+	for (i = 0; s2[i] != '\0' && i <= n; i++)
+	{
+		s3[j] = s1[i];
+		j++;
+	}
+	s3[j] = '\0';
+
+	return (s3);
+}
