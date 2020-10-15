7e116b1fa9a516abcbb4584f41931d31690e4ef5
Olivier <olivier.guyot.cu@gmail.com>
2020-10-15 18:54:45 +0000

    First commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		s[i] = 0;
 
 	return (s);
