41ac68dbdbcb83ef166cc60c721583ed9978b1cc
Olivier <olivier.guyot.cu@gmail.com>
2020-10-15 15:21:42 +0000

    First commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _calloc - _calloc
+ * @nmemb: unsigned
+ * @size: unsigned
+ * Return: unsigned
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(size * nmemb);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		s[i] = 0;
+
+	return (s);
+}
