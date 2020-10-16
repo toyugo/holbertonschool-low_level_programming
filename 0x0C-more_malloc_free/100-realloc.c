b62fb4a9eb62c8db876ced25240439b35557778b
Olivier <olivier.guyot.cu@gmail.com>
2020-10-16 08:59:16 +0000

    First commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @ptr: int
+ * @old_size: int 
+ * @new_size: int
+ * @size: the size of the memory to print
+ *
+ * Return: void* 
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+ 	void *tmp;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		tmp = malloc(new_size);
+		if (tmp == NULL)
+			return (NULL);
+		return (tmp);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	tmp = malloc(new_size);
+	free(ptr);
+	if (tmp == NULL)
+		return (NULL);
+	return (tmp);
+}
