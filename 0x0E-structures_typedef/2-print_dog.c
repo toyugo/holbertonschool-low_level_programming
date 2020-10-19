650301ee422c4c0747267363fb7c642aa17bf1c6
Olivier <olivier.guyot.cu@gmail.com>
2020-10-19 14:45:24 +0000

    First commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 	}
 }
