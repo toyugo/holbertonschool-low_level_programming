492c77081123a2e404e72d158403513185aa9534
Olivier <olivier.guyot.cu@gmail.com>
2020-10-19 12:55:05 +0000

    First commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog
+ * @name: name;
+ * @age: age;
+ * @owner: owner;
+ *
+ * Description: dog description
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
