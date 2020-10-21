1aa1fc3ea50a2daa2c61fe0d21d9e814547b1040
Olivier <olivier.guyot.cu@gmail.com>
2020-10-21 15:02:39 +0000

    First commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,23 @@
-#include <stdio.h>
-#include "holberton.h"
+#include "function_pointers.h"
+
 /**
- * @*array,: int
+ * int_index - int_index
+ * @array: int
+ * @size: size
+ * @cmp: cmp
  * Return: int
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
