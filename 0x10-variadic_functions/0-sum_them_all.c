d07efb88c1e695afe87ed5e2d7bb2f4e849bbb68
Olivier <olivier.guyot.cu@gmail.com>
2020-10-23 10:35:57 +0000

    First commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,19 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum;
 	unsigned int i;
+	unsigned int tps;
 	va_list a_list;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	sum = 0;
 	va_start(a_list, n);
-	for (i = 0; i != n; i++)
-		sum += va_arg(a_list, unsigned int);
 
+	for (i = 0; i != n; i++)
+	{
+		sum += va_arg(a_list, int);
+	}
 	return (sum);
 }
