5ea87bf37131876bb3b8ad72da34d242e61f121a
Olivier <olivier.guyot.cu@gmail.com>
2020-10-23 10:45:01 +0000

    First commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print_strings
+ * @separator: separator
+ * @n: constant to stop variadic
+ * Return: argument of type string
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list va;
+
+	if (separator == NULL)
+		return;
+	va_start(va, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(va, char*));
+		if (i == n - 1)
+			printf("\n");
+		else
+			printf(", ");
+	}
+}
