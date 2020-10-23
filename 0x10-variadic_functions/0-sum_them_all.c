e9f042a9077201b9c03695b140017729b7f551de
Olivier <olivier.guyot.cu@gmail.com>
2020-10-23 09:56:55 +0000

    First commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum_them_all
+ * @n: const
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum;
+	unsigned int i;
+	va_list a_list;
+
+	sum = 0;
+	va_start(a_list, n);
+	for (i = 0; i != n; i++)
+		sum += va_arg(a_list, unsigned int);
+
+	return (sum);
+}
