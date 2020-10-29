2432f0a94a9ffeccc5267c126247bb3006404b27
Olivier <olivier.guyot.cu@gmail.com>
2020-10-29 18:18:51 +0000

    First commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - add_node
+ * @str: str
+ * @head: head
+ * Return: Number of node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *tps;
+	int i = 0;
+	list_t *new;
+
+	tps = strdup(str);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = tps;
+	for (i = 0; tps[i]; i++)
+	;
+	new->len = i;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
