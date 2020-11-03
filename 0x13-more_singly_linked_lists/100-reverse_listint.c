c213448b67836bb347a16a33a81c1f11559f3741
Olivier <olivier.guyot.cu@gmail.com>
2020-11-03 21:49:38 +0000

    First commit

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverse_listint
+ * @head: listint_t
+ * Return: listint_t
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous;
+	listint_t *node;
+
+	if (head == NULL)
+		return (NULL);
+	previous = NULL;
+	node = *head;
+	while (node)
+	{
+		*head = (*head)->next;
+		node->next = previous;
+		previous = node;
+		node = *head;
+	}
+	*head = previous;
+	return (node);
+}
