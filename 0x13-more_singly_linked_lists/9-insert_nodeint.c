8ad8fff100681a39ef28d84b8fcd0d3baee87360
Olivier <olivier.guyot.cu@gmail.com>
2020-11-03 09:37:19 +0000

    First commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	i = 0;
+	if (*head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
