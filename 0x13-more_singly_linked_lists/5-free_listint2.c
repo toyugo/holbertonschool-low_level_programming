128f74dcb1317b57655b0f27162056272bd458bc
Olivier <olivier.guyot.cu@gmail.com>
2020-11-03 00:00:52 +0000

    First commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free_listint2
+ * @**head: listint_t
+ * Return: listint_t
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *node;
+	
+	node = *head;
+	while(node)
+	{
+		tmp = node;
+		node = node->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
