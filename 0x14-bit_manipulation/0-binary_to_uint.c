37b2e92d3b0b8a113757b192ae71f4db1431da66
Olivier <olivier.guyot.cu@gmail.com>
2020-11-05 13:09:26 +0000

    First commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * power - power
+ * @nb: cnb
+ * @pow: pow
+ * Return: const
+ */
+
+unsigned int power(unsigned int nb, unsigned int pow)
+{
+	int i;
+	unsigned int res;
+
+	res = nb;
+	if (pow == 0)
+		return (1);
+	for (i = 1; i < pow; i++)
+		res *= nb;
+	return (res);
+}
+
+
+/**
+ * binary_to_uint - binary_to_uint
+ * @b: const
+ * Return: const
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int len;
+	unsigned int sum;
+
+	i = 0;
+	while (b[i])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+	len = i - 1;
+	sum = 0;
+	for (i = 0; b[i]; i++)
+	{
+		sum += (b[i] - '0') * power(2, len);
+		len -= 1;
+	}
+
+	return (sum);
+}
