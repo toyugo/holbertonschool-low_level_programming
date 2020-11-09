11f176bbb38b8c5e1f2be7842621d5ea905a93a9
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 20:02:45 +0000

    First commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *append_text_to_file - append_text_to_file
+ *@filename: char
+ *@text_content: size
+ *Return: w
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	unsigned long int i;
+	int w;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	i = 0;
+	while (text_content[i])
+		i++;
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
