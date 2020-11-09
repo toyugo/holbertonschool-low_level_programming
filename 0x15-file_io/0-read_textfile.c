104ca80fdcc185525a9d09aa513731bf03e3d013
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 17:17:30 +0000

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,14 +21,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		close(fd);
 		free(buf);
 		return (0);
 	}
 	sz = read(fd, buf, letters);
 	if (sz == -1)
 	{
-		close(fd);
 		free(buf);
 		return (0);
 	}
@@ -36,7 +34,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	tw = write(STDOUT_FILENO, buf, sz);
 	if (tw == -1)
 	{
-		close(fd);
 		free(buf);
 		return (0);
 	}
