c504b02fc49a773a46f10f453bb26e6496710131
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 21:42:27 +0000

    First commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+
+/**
+ * main - Entry point
+ * @ac: number of argument
+ * @av: arguments.
+ * Return: 0 in success.
+ */
+int main(int ac, char **av)
+{
+	char buf[1024];
+	int e1, e, fd_from, fd_in, nb;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fd_in = open(av[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	if (fd_in == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	/*lire fichier et stocker dans buffer*/
+	printf("PRINT in \n");
+	while ((nb = read(fd_from, buf, 1024)) != 0)
+	{
+		printf("PRINT in %lu\n", nb);
+		write(fd_in, buf, nb);
+	}
+	e = close(fd_from);
+	e1 = close(fd_in);
+	if (e == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i", fd_in);
+		exit(100);
+	}
+	if (e1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i", fd_from);
+		exit(100);
+	}
+	return (1);
+}
