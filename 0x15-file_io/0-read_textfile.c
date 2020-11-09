241c8b70442c256fb84b17322788854fea667112
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 16:52:51 +0000

    add free

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,14 +20,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	sz = read(fd, buf, letters);
 	if (sz == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	buf[sz] = '\0';
 	tw = write(1, buf, sz);
 	if (tw == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	close(fd);
 	free(buf);
 	return (sz);
