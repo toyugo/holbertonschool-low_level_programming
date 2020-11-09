0ebd0bf44f72409f408fea6f5e50ebb62e69662e
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 19:52:20 +0000

    First commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,8 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i])
 		i++;
 	fd = open(filename, O_WRONLY);
+	if (fd == -1)
+		return (-1);
 	w = write(fd, text_content, i);
 	if (w == -1)
 		return (-1);
