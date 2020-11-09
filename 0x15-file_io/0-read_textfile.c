a51691fd76c9831e9984c38e29dc97843a63d46b
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 16:33:30 +0000

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,6 +13,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 
 	buf = malloc(sizeof(char) * letters);
+	if (filename == NULL)
+		return (0);
 	if (buf == NULL)
 		return (0);
 
@@ -20,8 +22,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	sz = read(fd, buf, letters);
-	if (sz == -1)
-		return (0);
 	write(1, buf, sz);
 	close(fd);
 	free(buf);
