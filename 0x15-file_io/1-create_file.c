9f433dd5b4a5fba328ceff207cd5bc0c62dc1c11
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 20:10:30 +0000

    Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	close(fd);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	i = 0;
 	while (text_content[i])
 		i++;
