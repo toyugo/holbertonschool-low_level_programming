7a06aeab7dd2b6f5361960d35376391b4158b01d
Olivier <olivier.guyot.cu@gmail.com>
2020-11-09 16:54:12 +0000

    First commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,12 +21,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
+		close(fd)
 		free(buf);
 		return (0);
 	}
 	sz = read(fd, buf, letters);
 	if (sz == -1)
 	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
@@ -34,6 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	tw = write(1, buf, sz);
 	if (tw == -1)
 	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
