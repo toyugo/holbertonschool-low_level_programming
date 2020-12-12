2d967789ac78231d93b82beec97b460c1379406c
Olivier <olivier.guyot.cu@gmail.com>
2020-12-12 23:46:27 +0000

    Fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	if (*head == NULL)
+	{
 		*head = new;
+		return (new);
+	}
 	else
 	{
 		ptr = *head;
