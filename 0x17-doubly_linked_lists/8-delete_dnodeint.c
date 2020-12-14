cbd76570fdd4880ecf63abeb7edff57b6bdce8ce
Olivier <olivier.guyot.cu@gmail.com>
2020-12-14 23:03:27 +0000

    First commit

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert_dnodeint_at_index
+ * @h: head
+ * @idx: index
+ * @n: n
+ * Return: sum
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *ptr;
+	unsigned int cp = 0;
+
+	ptr = *head;
+	while (cp <= index)
+	{	
+		ptr = ptr->next;
+		cp++;
+	}
+	if (*head == NULL || ptr == NULL)
+		return (-1);
+	return (1);
+}
