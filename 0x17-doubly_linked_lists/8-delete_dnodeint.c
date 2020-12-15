c1facba22975f9cc5bceffb1eb96ed624bb00826
Olivier <olivier.guyot.cu@gmail.com>
2020-12-15 00:11:42 +0000

    First commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	ptrm = *head;
 	ptrp = *head;
 	if (ptr->next != NULL)
-	ptr = ptr->next;
+		ptr = ptr->next;
 	if (ptrp->next->next != NULL)
 	{
 		ptrp = ptrp->next;
