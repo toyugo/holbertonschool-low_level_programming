0df135b70edf804d194b7d728f8c592fb702f79d
Olivier <olivier.guyot.cu@gmail.com>
2020-12-16 12:02:02 +0000

    Fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,45 +1,70 @@
 #include "lists.h"
 /**
+ * get_max_node - get_max_node
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+
+int get_max_node(dlistint_t **h)
+{
+	dlistint_t *ptr = *h;
+	unsigned int i = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
+/**
+ * goToIndex - goToIndex
+ * @h: pointer to head of list
+ * @idx: pointer to head of list
+ * Return: number of nodes
+ */  
+dlistint_t *goToIndex(dlistint_t **h, unsigned int idx)
+{
+	dlistint_t *ptr;
+	unsigned int cp = 0;
+
+	ptr = *h;
+
+	while (cp < idx)
+	{
+		ptr = ptr->next;
+		cp++;
+	}
+	return (ptr);
+}
+
+/**
  * delete_dnodeint_at_index - delete_dnodeint_at_index
- * @head: head
- * @index: index
+ * @h: head
+ * @idx: index
  * Return: sum
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *ptrM;
 	dlistint_t *ptr;
-	dlistint_t *ptrm;
-	dlistint_t *ptrp;
-	unsigned int cp = 1;
-
-	ptrm = *head;
-	if (ptrm->next == NULL || ptrm == NULL)
-		return (-1);
+	dlistint_t *ptrP;
+	unsigned int indexmax;
+	
 	ptr = *head;
-	ptrp = *head;
-	if (ptr->next != NULL)
-	{
-		ptrp = ptrp->next;
-		ptr = ptr->next;
-	}
-	if (ptrp->next != NULL)
-		ptrp = ptrp->next;
-	if (index == 0)
-	{
-		*head = ptr;
-		free(ptrm);
-		return (1);
-	}
-	while (cp < index)
-	{
-		ptr = ptr->next;
-		ptrm = ptrm->next;
-		ptrp = ptrp->next;
-		cp++;
-	}
-	ptrm->next = ptrp;
-	ptrp->prev = ptrm;
-	if (ptr != NULL)
-		free(ptr);
-	return (1);
+	indexmax = get_max_node(head);
+	if (index > indexmax || ptr == NULL)
+		return (0);
+	ptr = goToIndex(head, index);
+	ptrM = ptr->prev;
+	ptrP = ptr->next;
+	if (ptrM != NULL)
+		ptrM->next = ptrP;
+	else
+		*head = ptrP;
+	if (ptrP != NULL)
+		ptrP->prev = ptrM;
+	return (0);
 }
+
+
