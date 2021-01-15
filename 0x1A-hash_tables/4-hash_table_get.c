7007fb0f2b7aeeb61a45667c5d10ba8c3f841001
Olivier <olivier.guyot.cu@gmail.com>
2021-01-15 16:16:33 +0000

    Fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,10 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 		index = key_index((const unsigned char *)key, ht->size);
 		ptrNode = ht->array[index];
-		while(ptrNode)
+		while (ptrNode)
 		{
 			if (strcmp(ptrNode->key, key) == 0)
-				return (ptrNode->value); 
+				return (ptrNode->value);
 			ptrNode = ptrNode->next;
 		}
 		return (NULL);
