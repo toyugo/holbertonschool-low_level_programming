e685b79277dfe5c1b39894a912a5ad1386ffb1eb
Olivier <olivier.guyot.cu@gmail.com>
2021-01-15 15:33:54 +0000

    Fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,10 +10,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 	unsigned long int index;
-
+	unsigned long int i;
 	/*Get index*/
 	index = key_index((const unsigned char *)key, ht->size);
-	printf("index vault: %lu\n", index);
+	/*printf("index vault: %lu\n", index);*/
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+	}
 	/*Add AddNewNodeAtFirst*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
