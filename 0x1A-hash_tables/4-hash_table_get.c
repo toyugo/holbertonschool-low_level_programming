ef319f6d2e0c14ab183739029a0c1f00d8eb50c8
Olivier <olivier.guyot.cu@gmail.com>
2021-01-15 16:16:10 +0000

    Fix

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - hash_table_get
+ * @ht: hash table
+ * @key: key
+ * Return: chars associated value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+		unsigned long int index;
+		hash_node_t *ptrNode;
+
+		index = key_index((const unsigned char *)key, ht->size);
+		ptrNode = ht->array[index];
+		while(ptrNode)
+		{
+			if (strcmp(ptrNode->key, key) == 0)
+				return (ptrNode->value); 
+			ptrNode = ptrNode->next;
+		}
+		return (NULL);
+}
