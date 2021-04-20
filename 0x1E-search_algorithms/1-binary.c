26f2b2ac44d8bb6cb88f245c521db68fc206c5a2
Toyugo <2490@holbertonschool.com>
2021-04-20 16:27:47 +0000

    f

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,35 @@
-#!/usr/bin/python3
+#include "search_algos.h"
+
+/**
+  * binary_search - Searches for a value in a sorted array
+  * @array: A pointer to the first element.
+  * @size: The number of elements
+  * @value: The value to search
+  * Return: return value
+  */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+	size_t left;
+	size_t right;
+	size_t m;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		m = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+	return (-1);
+}
